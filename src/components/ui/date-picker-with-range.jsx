e2adc09fc7a1c0b8279f62114f57b46c8743e2cf
BrenodePaiva <brenopaiva552@gmail.com>
2026-08-31 13:20:15 -0300

    feat: add date picker with range component

---
+++ src/components/ui/date-picker-with-range.jsx
@@ -0,0 +1,41 @@
+'use client'
+
+import { format } from 'date-fns'
+
+import { Button } from '@/components/ui/button'
+import { Calendar } from '@/components/ui/calendar'
+import { Field, FieldLabel } from '@/components/ui/field'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
+
+export const DatePickerWithRange = ({ value, onChange }) => {
+  return (
+    <Field className="mx-auto w-44">
+      <FieldLabel htmlFor="date-picker-simple">Date</FieldLabel>
+      <Popover>
+        <PopoverTrigger
+          render={
+            <Button
+              variant="outline"
+              id="date-picker-simple"
+              className="justify-start font-normal"
+            >
+              {value ? format(value, 'PPP') : <span>Pick a date</span>}
+            </Button>
+          }
+        />
+        <PopoverContent className="w-auto p-0" align="start">
+          <Calendar
+            mode="single"
+            selected={value}
+            onSelect={onChange}
+            defaultMonth={value}
+          />
+        </PopoverContent>
+      </Popover>
+    </Field>
+  )
+}
