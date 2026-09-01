36e6bc00f8b6e85ca11257ec29402a11d7520df6
BrenodePaiva <brenopaiva552@gmail.com>
2026-09-01 16:09:46 -0300

    feat: ensure user balance is shown on home page

---
+++ src/components/balance.jsx
@@ -0,0 +1,53 @@
+import { useQuery } from '@tanstack/react-query'
+import {
+  PiggyBankIcon,
+  TrendingDownIcon,
+  TrendingUpIcon,
+  WalletIcon,
+} from 'lucide-react'
+import { useSearchParams } from 'react-router'
+
+import { useAuthContext } from '@/contexts/auth'
+import { UserService } from '@/services/user'
+
+import BalanceItem from './balance-item'
+
+const Balance = () => {
+  const [searchParams] = useSearchParams()
+  const { user } = useAuthContext()
+  const { data } = useQuery({
+    queryKey: ['balance', user.id],
+    queryFn: async () => {
+      const from = searchParams.get('from')
+      const to = searchParams.get('to')
+      return await UserService.getBalance({ from, to })
+    },
+  })
+  console.log({ data })
+  return (
+    <div className="grid grid-cols-2 grid-rows-2 gap-6">
+      <BalanceItem
+        label="Saldo"
+        amount={data?.balance}
+        icon={<WalletIcon size={16} />}
+      />
+      <BalanceItem
+        label="Ganhos"
+        amount={data?.earnings}
+        icon={<TrendingUpIcon size={16} className="text-primary-green" />}
+      />
+      <BalanceItem
+        label="Gastos"
+        amount={data?.expenses}
+        icon={<TrendingDownIcon size={16} className="text-primary-red" />}
+      />
+      <BalanceItem
+        label="Investimentos"
+        amount={data?.investiments}
+        icon={<PiggyBankIcon size={16} className="text-primary-blue" />}
+      />
+    </div>
+  )
+}
+
+export default Balance
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,7 @@
 import { PlusIcon } from 'lucide-react'
 import { Navigate } from 'react-router'
 
+import Balance from '@/components/balance'
 import DateSelection from '@/components/date-selection'
 import Header from '@/components/header'
 import { Button } from '@/components/ui/button'
@@ -16,7 +17,7 @@ const HomePage = () => {
   return (
     <>
       <Header />
-      <div className="p-8">
+      <div className="space-y-6 p-8">
         <div className="flex items-center justify-between">
           <h2 className="text-2xl font-bold">Dashboard</h2>
           <div className="flex items-center gap-2">
@@ -26,6 +27,10 @@ const HomePage = () => {
               Nova transaçãos
             </Button>
           </div>
+        </div>
+
+        <div className="grid grid-cols-[2fr,1fr]">
+          <Balance />
         </div>
       </div>
     </>
