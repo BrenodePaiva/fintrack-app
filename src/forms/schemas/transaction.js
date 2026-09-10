3234527a54ed44b275a695dd70ebf3f33a821d31
BrenodePaiva <brenopaiva552@gmail.com>
2026-09-10 11:40:03 -0300

    feat: add edit transaction form hooks and schemas

--- src/forms/schemas/transaction.js
+++ src/forms/schemas/transaction.js
@@ -10,3 +10,7 @@ export const createTransactionFormSchema = z.object({
     error: 'O tipo deve ser EARNING, EXPENSE ou INVESTMENT.',
   }),
 })
+
+export const editTransactionFormSchema = createTransactionFormSchema.extend({
+  id: z.uuid(),
+})
