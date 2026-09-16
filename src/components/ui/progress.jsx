3039da8b652cf11c0365d925518a8d9e6779897d
BrenodePaiva <brenopaiva552@gmail.com>
2026-09-16 10:51:16 -0300

    feat: add Progress component

---
+++ src/components/ui/progress.jsx
@@ -0,0 +1,29 @@
+import { Progress as ProgressPrimitive } from '@base-ui/react/progress'
+
+import { cn } from '@/lib/utils'
+
+function Progress({ className, value, ...props }) {
+  return (
+    <ProgressPrimitive.Root
+      data-slot="progress"
+      value={value}
+      className={cn(
+        'bg-primary/20 relative h-2 w-full overflow-hidden rounded-full',
+        className
+      )}
+      {...props}
+    >
+      <ProgressPrimitive.Track
+        data-slot="progress-track"
+        className="size-full overflow-hidden"
+      >
+        <ProgressPrimitive.Indicator
+          data-slot="progress-indicator"
+          className="bg-primary h-full transition-all"
+        />
+      </ProgressPrimitive.Track>
+    </ProgressPrimitive.Root>
+  )
+}
+
+export { Progress }
